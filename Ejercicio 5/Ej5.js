7e4486e72ca6b6071c303d14fd122a26ef5a3819
Javier Galvan <javiergalvanpoley@fp.iesromerovargas.com>
2022-04-27 14:09:15 +0200

    Simplificación del if y eliminación de variables innecesarias

--- Ejercicio 5/Ej5.js
+++ Ejercicio 5/Ej5.js
@@ -1,35 +1,20 @@
 function Ejercicio5(){
-
-    
-
     var edad = parseInt(document.getElementById("pal1").value);
     var mensaje  ="";
-
-    if(edad > 100){
+    if(edad > 100 || edad<0){
         mensaje+= "No válido";
     }
-    else if(edad < 0){
-        mensaje+= "No válido";
-    }else{
-        if (edad > 18) {
+    else{
+        if (edad >= 18) 
+        {
             mensaje+= "Puedes conducir"
         }
-        else{
-            if (edad == 18) {
-                mensaje+= "Puedes conducir"
-            }
-            else{
-                if(e > y){
-                    mensaje+= "No puedes conducir";
-                }
-            }
+        else
+        {            
+            mensaje+= "No puedes conducir";                
         }
     }
-
-    
-
-    alert(s);
-
+    alert(mensaje);
+}
    
 
-}
